3b6259d6a9e69d3f04c653f50074d1392a2174f0
DESKTOP-7UNACA3\CW <khubaibwaheed1995@gmail.com>
2022-08-22 21:28:47 +0500

    no message

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -709,7 +709,7 @@
             <div class="info-box  mb-4">
               <i class="bx bx-envelope"></i>
               <h3>Email Us</h3>
-              <p>khubaib@zymok.com</p>
+              <p>khubaibw24@gmail.com</p>
             </div>
           </div>
 
