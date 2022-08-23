09a19382deba95626dac5ac23cf85cc72ba946f4
DESKTOP-7UNACA3\CW <khubaibwaheed1995@gmail.com>
2022-08-23 18:23:53 +0500

    no message

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -63,7 +63,7 @@
 <body class="body">
 
   <!-- ======= Top Bar ======= -->
-  <section id="topbar" style="z-index: 100" class="d-flex align-items-center">
+  <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
         <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:khubaib@zymok.com">khubaib@zymok.com</a></i>
